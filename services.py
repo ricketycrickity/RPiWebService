6578ae6b0e45ac4169295838e0dc3d1b9f88102e
James Heiman <ricjames@vt.edu>
2020-04-16 17:03:53 -0400

    Created different url paths and able to read from url

--- services.py
+++ services.py
@@ -1,28 +1,41 @@
 #! usr/bin/env python3
-from flask import Flask
+from flask import Flask, request
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
-from tokens import *
+from ServicesKeys import *
+import requests
 import sys
 
 app = Flask(__name__)
 auth = HTTPBasicAuth()
 
 users = {
-    "john": generate_password_hash("hello"),
-    "susan": generate_password_hash("bye")
+    "username": generate_password_hash("admin"),
+    "password": generate_password_hash("secret")
 }
-
+"""
 @auth.verify_password
 def verify_password(username, password):
     if username in users:
         return check_password_hash(users.get(username), password)
+    print('>Could not verify your access level for that URL. You have to login with proper credentials')
     return False
-
+"""
 @app.route('/')
-@auth.login_required
-def index():
-    return "Hello, %s!" % auth.username()
+def basicReply():
+    return "Hello!"
+
+
+@app.route('/Canvas')
+#@auth.login_required
+def getCanvas():
+    filename = request.args.get('file')
+    return "<h1>Going to Canvas to find %s!</h1>" % filename
+
+@app.route('/Marvel')
+def getMarvel():
+    storynum = request.args.get('story')
+    return "<h1>Going to Marvel to find story #%s!</h1>" % storynum
 
 if __name__ == '__main__':
     # standard port number for web services
